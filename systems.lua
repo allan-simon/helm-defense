ad4dc5431d585bb4cf7011946a7c1eb280d35523
Allan SIMON <allan.simon@supinfo.com>
2023-04-04 22:59:00 +0200

    fix units never reaching the exact position in their squad
    fixed by introducing a 'tolerance' threshold for a 'you've arrived close enough to
    your target so stop moving

--- systems.lua
+++ systems.lua
@@ -107,13 +107,24 @@ function PlayerActionSystem:playerMove(player)
     end
 end
 
-local function goTowardShape(e, targetShape) 
+local function goTowardShape(e, targetShape, tolerance) 
+    tolerance = tolerance or 0
+
     local tX, tY = targetShape:center()
     local eX, eY = e.tangible.shape:center()
 
     -- we get the angle between the follower and its target
     local dx = tX - eX
     local dy = tY - eY
+
+    -- no need to for perfect precision
+    -- if you reached your destination up to the tolerance threshold
+    -- we stop you there
+    if ((dx*dx + dy*dy) <  tolerance*tolerance) then
+        e.velocity.x = 0
+        e.velocity.y = 0
+        return
+    end
 
     local angle = atan2(dy, dx) + math.pi * 0.5
 
@@ -138,7 +149,16 @@ function FollowSquadSystem:followSquad()
             if unit:has('cantMove') then
                 goto continue
             end
-            goTowardShape(unit, unitRank)
+            -- we allow a tolerance of 1
+            -- otherwise the units would never quite reach the exact point
+            -- and would move back and forth continously
+            goTowardShape(unit, unitRank, 1)
+            -- if the unit has stopped moving it means it has its destination
+            -- so we realign it with the squad direction to have the "soldier in line"
+            -- feeling
+            if unit.velocity.x == 0 and unit.velocity.y == 0 then
+                unit.tangible.shape:setRotation(e.tangible.shape:rotation())
+            end
             ::continue::
         end
     end
