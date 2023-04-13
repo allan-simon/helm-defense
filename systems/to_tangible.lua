39f579333f68bd5e0e787dd2d13b20bc0a3df523
Allan SIMON <allan.simon@supinfo.com>
2023-04-13 22:40:20 +0200

    squad can now have several rows

--- systems/to_tangible.lua
+++ systems/to_tangible.lua
@@ -28,72 +28,102 @@ function ToTangibleSystem:toTangible()
         :remove("futureTangible")
     end
 
+    ------------------------------
+    --  Case for Squads ,
+    --  i.e calculate their 'shape' depending on the units
+    --  composing them
+    -----------------------------
+
     for _, e in ipairs(self.squads) do
         local sumX = 0
         local sumY = 0
 
-        local sumWidth = 0
-        local sumHeight = 0
 
         -- TODO: compute the "circular mean"
         -- local _sumAngleX = 0
         -- local _sumAngleY = 0
 
-        local units = {}
+        local unitsKeys = {}
+        local unitsEntities = {}
         for _, unitKey in ipairs(e.squad.units) do
             local unit = world:getEntityByKey(unitKey)
             -- remove dead units
-            if unit == nil then
-                goto continue
+            if unit ~= nil then
+                unitsKeys[#unitsKeys+1] = unitKey
+                unitsEntities[#unitsEntities+1] = unit
             end
-            units[#units+1] = unitKey
-
-
-            local x, y = unit.tangible.shape:center()
-            sumX = sumX + x
-            sumY = sumY + y
-
-            sumWidth = sumWidth + unit.drawable.width
-            sumHeight = sumHeight + unit.drawable.height
-
-            ::continue::
         end
-        e.squad.units = units
-        if #units == 0 then
+        local numberUnits = #unitsKeys
+        -- as in the loop above we've removed the dead units
+        -- we here check that the squad still has unit alive
+        -- if not, we consider the squad itself as decimated
+        if numberUnits == 0 then
             e:ensure('killed')
             goto bigContinue
         end
 
-        local numberUnits = #e.squad.units
+        e.squad.units = unitsKeys
+
+        local numberCols = 8
+        local numberRows = math.ceil(numberUnits / numberCols)
+
+        local unitMaxWidth = 0
+        local unitMaxHeight = 0
+
+        for _, unit in  ipairs(unitsEntities) do
+            local x, y = unit.tangible.shape:center()
+            sumX = sumX + x
+            sumY = sumY + y
+
+            unitMaxWidth = math.max(unitMaxWidth, unit.drawable.width)
+            unitMaxHeight = math.max(unitMaxHeight, unit.drawable.height)
+
+        end
+
+
         local gap = 2
         -- we add a gap of 2 between each units
-        local totalWidth = sumWidth + ((numberUnits - 1) * gap)
-        local totalHeight = sumHeight / numberUnits
+        local totalWidth = (unitMaxWidth + gap) * numberCols
+        local totalHeight = (unitMaxHeight + gap) * numberRows
 
 
         local rectangle = HC.rectangle(
+            -- the gravity center of the squad
+            -- is set to the average point of the units
             (sumX/numberUnits) - totalWidth* 0.5,
             (sumY/numberUnits) - totalHeight* 0.5,
+            --
             totalWidth,
             totalHeight
         )
+        -- hack: we add _ghost to the shape like this
+        -- as it's not a Entity so we can't add to it a Component
         rectangle._ghost = true
+
         local centerX, centerY = rectangle:center()
-        print(centerX, centerY)
 
         local widthOffset = centerX - (totalWidth * 0.5)
-        print(widthOffset, "totalWidth", totalWidth)
+        local heightOffset = centerY - (totalHeight * 0.5)
+
         local unitRanks = {}
-        for _, unitKey in ipairs(e.squad.units) do
-            local unit = world:getEntityByKey(unitKey)
-            local width = unit.drawable.width;
+        local remainingUnits = #unitsEntities
+        local position = 0
+        for i = 1,numberRows do
+            local numberUnitInThatRow = math.min(remainingUnits, numberCols)
+            for j = 1,numberUnitInThatRow do
+                remainingUnits = remainingUnits - 1
 
-            local point = HC.point(widthOffset + width*0.5  , centerY)
-            point._key = unitKey
-            point._ghost = true
-            widthOffset = widthOffset + width + gap
+                local point = HC.point(
+                     widthOffset + (unitMaxWidth + gap)*(j-1) + unitMaxWidth*0.5,
+                     heightOffset + (unitMaxHeight + gap)*(i -1) + unitMaxHeight*0.5
+                )
+                position = position + 1
+                point._key = unitsKeys[position]
 
-            unitRanks[#unitRanks + 1] = point
+                point._ghost = true
+
+                unitRanks[#unitRanks + 1] = point
+            end
         end
         rectangle._key = e.key.value
 
